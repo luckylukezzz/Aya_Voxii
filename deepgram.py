6c657e8902f947cdab96b0899502fa12364d60f4
luckylukezzz <50476499+luckylukezzz@users.noreply.github.com>
2023-10-12 12:01:59 +0530

    openapi con done

--- deepgram.py
+++ deepgram.py
@@ -1,19 +1,15 @@
+from deepgram import Deepgram
+from asyncio import run 
+DEEPGRAM_API_KEY='aaf11ae92027d33bb893d307bac9619c4a16a825a'
+deepgram = Deepgram(DEEPGRAM_API_KEY)
 
-import deepgram
-import asyncio
-dg_client = deepgram.Deepgram({ 
-  "api_key": 'aaf11ae92027d33bb893d307bac9619c4a16a825a', 
-  "api_url": "http://localhost:8080/v1/listen" 
-})
 async def transcribe_audio():
     source = {'url': "./rec.wav"}
     try:
-        response = await dg_client.transcription.prerecorded(source, {'punctuate': True})
+        response = await deepgram.transcription.prerecorded(source, {'punctuate': True})
         print(response)
     except Exception as e:
         print(f"An error occurred: {e}")
 
 
-
-asyncio.run(transcribe_audio())
-print(dir(deepgram))
+run(transcribe_audio())
